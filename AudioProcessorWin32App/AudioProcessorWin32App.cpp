643b396afca04b8607c600d120c5691a4050f1a7
amWRit <derasrocks@gmail.com>
2024-12-13 20:46:39 +0545

    first test of opening input file path

--- AudioProcessorWin32App/AudioProcessorWin32App.cpp
+++ AudioProcessorWin32App/AudioProcessorWin32App.cpp
@@ -3,6 +3,9 @@
 
 #include "framework.h"
 #include "AudioProcessorWin32App.h"
+#include <commdlg.h>  // For file dialogs
+#include "FileHandler.h"
+
 
 #define MAX_LOADSTRING 100
 
@@ -11,16 +14,23 @@ HINSTANCE hInst;                                // current instance
 WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
 WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
 
+HWND hwndButton;
+HWND hwndInputFilePathBox;
+HWND hwndOutputFilePathBox;
+wchar_t g_selectedFilePath[260] = { 0 }; // Global variable
+
 // Forward declarations of functions included in this code module:
 ATOM                MyRegisterClass(HINSTANCE hInstance);
 BOOL                InitInstance(HINSTANCE, int);
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
 
+void OpenFileDialog(HWND hWnd);
+
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+    _In_opt_ HINSTANCE hPrevInstance,
+    _In_ LPWSTR    lpCmdLine,
+    _In_ int       nCmdShow)
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
@@ -33,7 +43,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
     MyRegisterClass(hInstance);
 
     // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
+    if (!InitInstance(hInstance, nCmdShow))
     {
         return FALSE;
     }
@@ -52,7 +62,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
         }
     }
 
-    return (int) msg.wParam;
+    return (int)msg.wParam;
 }
 
 
@@ -68,17 +78,17 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 
     wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_AUDIOPROCESSORWIN32APP));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_AUDIOPROCESSORWIN32APP);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDC_AUDIOPROCESSORWIN32APP));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_AUDIOPROCESSORWIN32APP);
+    wcex.lpszClassName = szWindowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
     return RegisterClassExW(&wcex);
 }
@@ -95,20 +105,20 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Store instance handle in our global variable
+    hInst = hInstance; // Store instance handle in our global variable
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+    HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+    if (!hWnd)
+    {
+        return FALSE;
+    }
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
 
-   return TRUE;
+    return TRUE;
 }
 
 //
@@ -125,31 +135,50 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
     {
+    case WM_CREATE:
+        // Create the Open file button
+        hwndButton = CreateWindowW(L"BUTTON", L"Open file",
+            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            150, 100, 100, 40, hWnd, (HMENU)1, hInst, NULL);
+
+        // Create the text box
+        hwndInputFilePathBox = CreateWindowW(L"EDIT", L"", WS_CHILD | WS_VISIBLE | WS_BORDER | ES_AUTOHSCROLL,
+            150, 50, 700, 25, hWnd, (HMENU)2, hInst, NULL);  // ES_AUTOHSCROLL adds horizontal scrolling if needed
+        break;
+
     case WM_COMMAND:
+    {
+        int wmId = LOWORD(wParam);
+        // Parse the menu selections:
+        switch (wmId)
         {
-            int wmId = LOWORD(wParam);
-            // Parse the menu selections:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
+        case 1:  // Open File Button click event (ID 1)
+        {
+            // Open the file dialog
+            static FileHandler fileHandler;
+            fileHandler.OpenFileDialog(hWnd);
+            SetWindowTextW(hwndInputFilePathBox, fileHandler.GetInputFilePath());
+            break;
         }
-        break;
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+    break;
     case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
-            EndPaint(hWnd, &ps);
-        }
-        break;
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        // TODO: Add any drawing code that uses hdc here...
+        EndPaint(hWnd, &ps);
+    }
+    break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -178,3 +207,54 @@ INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
     }
     return (INT_PTR)FALSE;
 }
+
+// Open file dialog boox
+// Help: https://cplusplus.com/forum/windows/169960/
+// // NOT USED - just for backup
+//void OpenFileDialog(HWND hWnd)
+//{
+//    OPENFILENAMEW ofn;       // Common dialog box structure
+//    wchar_t szFile[260];     // Buffer for file name
+//
+//    // Initialize OPENFILENAMEW structure
+//    ZeroMemory(&szFile, sizeof(szFile));
+//    ZeroMemory(&ofn, sizeof(ofn));
+//    ofn.lStructSize = sizeof(ofn);
+//    ofn.hwndOwner = hWnd;
+//    ofn.lpstrFile = szFile;
+//    ofn.nMaxFile = sizeof(szFile);
+//    ofn.lpstrFilter = L"Wave Files (*.wav)\0*.wav\0AIFF Files (*.aiff)\0*.aiff\0All Supported Files (*.wav;*.aiff)\0*.wav;*.aiff\0";
+//    ofn.nFilterIndex = 1;
+//    ofn.lpstrFileTitle = NULL;
+//    ofn.nMaxFileTitle = 0;
+//    ofn.lpstrInitialDir = NULL;
+//    ofn.lpstrTitle = L"Select a File";
+//    ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
+//
+//    // Display the file dialog
+//    if (GetOpenFileNameW(&ofn) == TRUE)
+//    {
+//        // File selected, show the path in the message box
+//        MessageBoxW(hWnd, ofn.lpstrFile, L"Selected File", MB_OK);
+//        // File selected; set its path in the edit control
+//        SetWindowTextW(hwndFilePathBox, ofn.lpstrFile);
+//    }
+//    else
+//    {
+//        // Handle the cancellation or error
+//        DWORD error = CommDlgExtendedError();
+//        if (error == 0)
+//        {
+//            // User canceled the dialog
+//            MessageBoxW(hWnd, L"File selection canceled.", L"Info", MB_OK | MB_ICONINFORMATION);
+//        }
+//        else
+//        {
+//            // An error occurred
+//            wchar_t errorMessage[256];
+//            swprintf_s(errorMessage, L"Error occurred. Code: 0x%08X", error);
+//            MessageBoxW(hWnd, errorMessage, L"Error", MB_OK | MB_ICONERROR);
+//        }
+//    }
+//
+//}
